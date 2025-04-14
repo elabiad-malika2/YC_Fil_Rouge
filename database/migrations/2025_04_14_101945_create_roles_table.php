438635eafb7cb2b7110338705819503c909b02ce
Malika <elabiadmalika64@gmail.com>
2025-04-14 11:39:42 +0100

    remplissage des migration user et role

--- database/migrations/2025_04_14_101945_create_roles_table.php
+++ database/migrations/2025_04_14_101945_create_roles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->enum('name', ['admin', 'enseignant', 'etudiant'])->unique();
             $table->timestamps();
         });
     }
