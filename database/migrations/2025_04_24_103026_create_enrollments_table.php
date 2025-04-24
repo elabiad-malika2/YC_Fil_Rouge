954151b570c6f0d750cd87fc0fdaff0495b161cb
Malika <elabiadmalika64@gmail.com>
2025-04-24 11:36:01 +0100

    remplire la migration d'enrollements

--- database/migrations/2025_04_24_103026_create_enrollments_table.php
+++ database/migrations/2025_04_24_103026_create_enrollments_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 8, 2);
+            $table->string('payment_status')->default('pending'); 
+            $table->string('payment_id')->nullable(); 
             $table->timestamps();
         });
     }
