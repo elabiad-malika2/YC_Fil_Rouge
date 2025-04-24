903d88db42a47d24266f30c7b7d3f59cff21e652
Malika <elabiadmalika64@gmail.com>
2025-04-24 11:38:35 +0100

    faire les relations necessaires dans le modele enrollement

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Enrollment extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'amount',
+        'payment_status',
+        'payment_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
 }
